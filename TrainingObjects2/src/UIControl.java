dbacf7c3ac16835dd7da9491394de0b95685b5cf
Butch Paolo Madahan <butchpaolom@gmail.com>
2020-09-04 15:22:43 +0800

    Another Objects from Training

---
+++ TrainingObjects2/src/UIControl.java
@@ -0,0 +1,17 @@
+public abstract class UIControl {
+  private boolean isEnabled = true;
+
+  public abstract void render();
+
+  public final void enable() {
+    isEnabled = true;
+  }
+
+  public void disable() {
+    isEnabled = false;
+  }
+
+  public boolean isEnabled() {
+    return isEnabled;
+  }
+}
